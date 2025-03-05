c6064f0514e1cf0da8d77955bc73b1938999bfe7
Bintang Ramadhan Octavianto <130009454+bintangro27@users.noreply.github.com>
2025-03-05 23:47:49 +0700

    menambahkan alert Username atau password salah jika gagal saat login

--- login.php
+++ login.php
@@ -145,7 +145,12 @@ if(isset($_POST["login"])){
             <h1 class="text-3xl font-bold mb-4 text-gray-800 bounce-in text-center">Login</h1>
             <p class="text-gray-700 mb-6 text-center">Login ini hanya untuk penggunaan admin. Masukkan kredensial Anda untuk mengakses dashboard admin.</p>
             <?php if(isset($error)) : ?>
-            <!-- <p style="color: red">username / password salah</p> -->
+              <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
+                <strong class="font-bold">Error!</strong>
+                <span class="block sm:inline">Username atau password salah.</span>
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <a href="login.php">  <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 5.652a1 1 0 00-1.414 0L10 8.586 7.066 5.652a1 1 0 10-1.414 1.414L8.586 10l-2.934 2.934a1 1 0 101.414 1.414L10 11.414l2.934 2.934a1 1 0 001.414-1.414L11.414 10l2.934-2.934a1 1 0 000-1.414z"/></svg></a>
+                </span>
            <?php endif; ?>
             <form action="" method="post" class="space-y-4">
                 <div>
